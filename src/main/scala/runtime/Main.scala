5030d7dcbc137cec3ac69b442db8c8059a097471
Treyvian <davide.pozzoli@gmail.com>
2021-11-30 18:47:41 +0100

    Second day trying to format the input

--- src/main/scala/runtime/Main.scala
+++ src/main/scala/runtime/Main.scala
@@ -1,6 +1,7 @@
 package runtime
 
 import org.apache.spark.{SparkConf, SparkContext};
+import scala.collection.mutable.ArrayBuffer
 import logic.LogStruct;
 
 object HelloScala {
@@ -8,21 +9,18 @@ object HelloScala {
   def main(args: Array[String]) {
 
     //Create a SparkContext to initialize Spark
-    val conf = new SparkConf()
-    conf.setMaster("local")
-    conf.setAppName("Log Analysis")
+    val conf = new SparkConf().setAppName("Log Analysis").setMaster("local")
     val sc = new SparkContext(conf)
 
     // Load the text into a Spark RDD, which is a distributed representation of each line of text
     val textFile = sc.textFile("src/main/scala/resources/log.txt")
 
-    //word count
+    // Working with rdd
+    val logs = textFile.flatMap(line => line.split('\n'))
 
-    val counts = textFile.flatMap(line => line.split('\n'))
+    logs.foreach(println)
 
-    counts.foreach(println)
-    System.out.println("Total words: " + counts.count());
-
+    //System.out.println("Total words: " + logs.count());
     //counts.saveAsTextFile("/tmp/shakespeareWordCount")
 
     sc.stop()
