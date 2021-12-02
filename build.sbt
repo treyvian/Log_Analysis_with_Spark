e7dc64aa9c35537db67d71d6a2ce3044aa2440c3
Treyvian <davide.pozzoli@gmail.com>
2021-12-02 18:58:50 +0100

    First attempt with regex

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.13.7"
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
 libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.0"
--- src/main/scala/runtime/Main.scala
+++ src/main/scala/runtime/Main.scala
@@ -1,24 +1,32 @@
 package runtime
 
-import org.apache.spark.{SparkConf, SparkContext};
-import scala.collection.mutable.ArrayBuffer
-import logic.LogStruct;
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.regexp_extract
+
 
 object HelloScala {
 
   def main(args: Array[String]) {
 
     //Create a SparkContext to initialize Spark
-    val conf = new SparkConf().setAppName("Log Analysis").setMaster("local")
-    val sc = new SparkContext(conf)
+    val sc = SparkSession.builder()
+      .master("local[1]")
+      .appName("Log_Analysis")
+      .getOrCreate();
+    //val conf = new SparkConf().setAppName("Log Analysis").setMaster("local")
+    //val sc = new SparkContext(conf)
 
     // Load the text into a Spark RDD, which is a distributed representation of each line of text
-    val textFile = sc.textFile("src/main/scala/resources/log.txt")
+    //val log_lines = sc.textFile("src/main/scala/resources/log.txt")
+    val df = sc.read.option("header", "false").csv("src/main/scala/resources/log.txt")
 
-    // Working with rdd
-    val logs = textFile.flatMap(line => line.split('\n'))
+    val resultDF = df.withColumn("host",regexp_extract(col("_c0"), "\\[\\d{2}/\\w{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} [+-]\\d{4}\\]", 0))
 
-    logs.foreach(println)
+    resultDF.show()
+    resultDF.foreach(println)
+    //resultDF.foreach(println)
 
     //System.out.println("Total words: " + logs.count());
     //counts.saveAsTextFile("/tmp/shakespeareWordCount")
