672f0a5bad2c8c19e6d126fe9fa8ea6e7e23d93b
Treyvian <davide.pozzoli@gmail.com>
2021-12-15 19:08:26 +0100

    Dealing with bad format in input

--- src/main/scala/runtime/Main.scala
+++ src/main/scala/runtime/Main.scala
@@ -1,9 +1,8 @@
 package runtime
 
-import org.apache.spark.{SparkConf, SparkContext}
+//import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.functions.regexp_extract
+import org.apache.spark.sql.functions.{col, regexp_extract}
 
 
 object Main {
@@ -20,28 +19,22 @@ object Main {
 
     // Load the text into a Spark RDD, which is a distributed representation of each line of text
     //val log_lines = sc.textFile("src/main/scala/resources/log.txt")
-    val df = sc.read.option("header", "false").csv("src/main/scala/resources/log10.txt")
+    val df = sc.read.option("header", value = false)
+                    .option("delimiter", "\n")
+                    .csv("src/main/scala/resources/log10.txt")
 
-    val resultDF = df.withColumn("host",regexp_extract(col("_c0"),
+    val regexDf = df.withColumn("host",regexp_extract(col("_c0"),
                         "^([^\\s]+\\s)",0))
                       .withColumn("timestamp",regexp_extract(col("_c0"),
                         "\\[\\d{2}/\\w{3}/\\d{4}:\\d{2}:\\d{2}:\\d{2} [+-]\\d{4}\\]", 0))
                       .withColumn("path",regexp_extract(col("_c0"),
-                        "\"\\w+\\s+([^\\s]+)\\s+HTTP\\S*",0))
-                      .withColumn("path",regexp_extract(col("_c0"),
-                        "\"\\w+\\s+([^\\s]+)\\s+HTTP\\S*",0))
+                        "[A-Z]+\\s+[\\d\\D]+\\sHTTP\\S+",0))
                       .withColumn("status",regexp_extract(col("_c0"),
                         "\"\\s+(\\d{3})",0))
                       .withColumn("content_size",regexp_extract(col("_c0"),
                         "\\s+(\\d+)\\s\"",0)).drop("_c0")
 
-
-    resultDF.show()
-    //resultDF.foreach(println)
-    //resultDF.foreach(println)
-
-    //System.out.println("Total words: " + logs.count());
-    //counts.saveAsTextFile("/tmp/shakespeareWordCount")
+    regexDf.show()
 
     sc.stop()
   }
